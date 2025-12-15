696c8ec12049e157da88571fe5e090d1f619ab40
KenjiTonnaer <Kenjitonnaer11@gmail.com>
2025-12-15 10:35:34 +0100

    seeder product update

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -16,24 +16,54 @@ class ProductFactory extends Factory
      */
     public function definition(): array
     {
-        $names = [
-            'Kenji SX Premium',
-            'Kenji MK-II',
-            'Kenji-X1000',
-            'KKKenji Deluxe',
-            'Xtreme Kenji Pro',
-        ];
-        $types = [
-            'beans',
-            'parts',
-            'machines',
+        $machines = [
+            'Barroc Espresso Deluxe Pro',
+            'Barroc Café Master 3000',
+            'Barroc Bean-to-Cup Premium',
+            'Barroc Automatic Espresso Maker',
+            'Barroc Cappuccino Express',
+            'Barroc Professional Barista Edition',
+            'Barroc Home Espresso System',
         ];
 
+        $beans = [
+            'Barroc Arabica Beans - Dark Roast',
+            'Barroc Robusta Blend - Medium',
+            'Barroc Ethiopian Single Origin',
+            'Barroc Colombian Supreme',
+            'Barroc Espresso Blend Premium',
+            'Barroc Brazil Santos',
+            'Barroc Italian Roast',
+        ];
+
+        $parts = [
+            'Barroc Portafilter Handle',
+            'Barroc Steam Wand Replacement',
+            'Barroc Brew Group Assembly',
+            'Barroc Water Tank',
+            'Barroc Drip Tray',
+            'Barroc Milk Frother Attachment',
+            'Barroc Grinder Burr Set',
+        ];
+
+        $types = ['beans', 'parts', 'machines'];
+        $type = $this->faker->randomElement($types);
+
+        $productName = match($type) {
+            'machines' => $this->faker->randomElement($machines),
+            'beans' => $this->faker->randomElement($beans),
+            'parts' => $this->faker->randomElement($parts),
+        };
+
         return [
-            'product_name' => $this->faker->randomElement($names),
-            'stock' => $this->faker->numberBetween(1, 100),
-            'price' => $this->faker->randomFloat(2, 10, 1000),
-            'type' => $this->faker->randomElement($types),
+            'product_name' => $productName,
+            'stock' => $this->faker->numberBetween(0, 30),
+            'price' => match($type) {
+                'machines' => $this->faker->randomFloat(2, 299, 1999),
+                'beans' => $this->faker->randomFloat(2, 4, 55),
+                'parts' => $this->faker->randomFloat(2, 15, 220),
+            },
+            'type' => $type,
         ];
     }
 }
